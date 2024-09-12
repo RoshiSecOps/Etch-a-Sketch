2c2c81d91e56ba3ac1c9a8ab5b7cd4ba9369b7a7
Ivan Popov <116061712+RoshiSecOps@users.noreply.github.com>
2024-09-11 22:31:45 -0700

    E-A-S Project
    - Created the css and js files.
    - Used for loops in JS to create the grid.
    - Used some styling and sizing to make the grid symmetric.
    - made the rows/colums flex and changed eachs' direction.

---
+++ etch-a-sketch.js
@@ -0,0 +1,16 @@
+const container = document.getElementById("Container");
+const rows = document.getElementById("gridRow");
+const colums = document.getElementById("gridColumn");
+
+let boxCount = 32
+for (i = 0; i < boxCount; i ++) {
+    let row = document.createElement("div");
+    container.appendChild(row).className = "gridRow";
+
+    for (r = 0; r < boxCount; r ++) {
+        let column = document.createElement("div");
+        row.appendChild(column).className = "gridColumn";
+    }
+}
+
+//colums.addEventListener('hover', )
